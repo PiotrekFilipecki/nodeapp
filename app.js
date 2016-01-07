24fed7bfa8d28c4bc840447d4384fca2b72323ca
Piotrek Filipecki <piotrekfilipecki@gmail.com>
2016-01-07 18:59:43 +0100

    mongodb exercise

--- app.js
+++ app.js
@@ -1,9 +1,42 @@
-var express = require('espress');
+//Module Dependencies
+var express = require('express');
+var stylus = require('stylus');
+var nib = require('nib');
 
+//Set Port Number
+var portnumber = 3000;
+
+//Initiate Express
 var app = express();
+console.log('Express framework initiated');
 
-app.get('/',function(req,res){
-	res.render('index.ejs',{title:"express app 1"});
-});
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
 
-app.listen(3000);
+//Set Views Folder
+app.set('views', __dirname + '/views');
+//Initiate Jade
+app.set('view engine', 'jade');
+console.log('Jade view engine initiated');
+
+//Stylus Middleware
+app.use(express.logger('dev'));
+app.use(stylus.middleware(
+  { src: __dirname + '/public'
+  , compile: compile
+  }
+))
+app.use(express.static(__dirname + '/public'));
+
+//Render Index Page
+app.get('/', function (req, res) {
+  res.render('index',
+  { title : 'Welcome' }
+  )
+})
+
+app.listen(portnumber);
+console.log('Server is now running on port '+portnumber);
